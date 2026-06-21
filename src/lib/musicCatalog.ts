707bd353662b01807c1eb0ec5b7248035d6adff9
runningwolf-srk <runningwolf1987@gmail.com>
2026-06-21 11:57:18 -0500

    Update musicCatalog.ts

--- src/lib/musicCatalog.ts
+++ src/lib/musicCatalog.ts
@@ -6,7 +6,22 @@ export type Track = {
 }
 
 export const musicCatalog: Track[] = [
-  { id: "track-1", title: "Song 1", artist: "RunningWolf", youtubeId: "" },
-  { id: "track-2", title: "Song 2", artist: "RunningWolf", youtubeId: "" },
-  { id: "track-3", title: "Song 3", artist: "RunningWolf", youtubeId: "" }
+  {
+    id: "track-1",
+    title: "Track 1",
+    artist: "RunningWolf",
+    youtubeId: "HNYdZTp1qNc"
+  },
+  {
+    id: "track-2",
+    title: "Track 2",
+    artist: "RunningWolf",
+    youtubeId: "UT1c5Q81kVk"
+  },
+  {
+    id: "track-3",
+    title: "Track 3",
+    artist: "RunningWolf",
+    youtubeId: "GMRfdPSNVZM"
+  }
 ]
